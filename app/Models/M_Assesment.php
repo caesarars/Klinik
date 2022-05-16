2b19f9b970b6612d041f6568f53f06a3cabdd755
Diokta <dioktaredho@gmail.com>
2022-05-16 23:15:32 +0700

    Revert "update"
    This reverts commit e052c02c80454b6ae070e4135828cacb0415ad7d.

--- app/Models/M_Assesment.php
+++ app/Models/M_Assesment.php
@@ -6,6 +6,10 @@ use CodeIgniter\Model;
 
 class M_Assesment extends Model
 {
+<<<<<<< HEAD:app/Models/M_Assessment.php
     protected $table = 'tb_assessment';
+=======
+    protected $table = 'assesment';
+>>>>>>> parent of 654dd2e (finalisasi):app/Models/M_Assesment.php
     protected $allowedFields = ['id', 'idPasien', 'idPerawat', 'tanggal', 'keluhanUtama', 'tekananDarah', 'frekuensiNadi', 'suhu', 'frekuensiNafas', 'skorNyeri', 'beratBadan', 'tinggiBadan', 'lingkarKepala'];
 }
